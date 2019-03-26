5fb8fa203f17398ed577d2d8f2d406e2d6c0a635
Timothy Cavinder <Timothycavinder@msn.com>
2019-03-26 14:39:47 -0400

    added database

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const flash = require('connect-flash')
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Load User Model
+require('./models/User');
+const User = mongoose.model('users');
+
+const users = require('./routes/users')
+//passport config
+require('./config/passport')(passport);
+//DB config
+const db = require('./config/database');
+
+mongoose.connect(db.mongoURI, {
+    useMongoClient: true,
+    useNewUrlParser: true
+}) 
+.then(() => console.log('MongoDB Connected . . .'))
+.catch(err => console.log(err));
 
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
+
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Express session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+//Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+})
 
 app.get('/', (req, res) => {
     const title = 'Log In or Sign Up';
@@ -24,6 +71,8 @@ app.get('/', (req, res) => {
 app.get('/welcome', (req,res) => {
    res.render('welcome')
     })
+
+    app.use('/users', users);
 
     const port = process.env.PORT || 5051;
 
